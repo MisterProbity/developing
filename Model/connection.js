cad3ed9934ee31fa242549fe9adcbcf9bc258cb4
waliu <waliuraheemprobity@gmail.com>
2023-09-03 15:52:51 +0100

    logoAdjust

--- Model/connection.js
+++ Model/connection.js
@@ -1,15 +1,18 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
+
+    host:"db4free.net",
+    user:"waliuraheem",
+    password: "probitymr",
+    database: "probity_libsys",
+    port: 3306
+
     // host: "localhost",
     // user: "root",
     // password: "",
     // database:"probity_libsys"
-     host:"db4free.net",
-     user:"waliuraheem",
-     password: "probitymr",
-     database: "probity_libsys",
-     port: 3306
+    
 
 });
 connection.connect();
